410da7a528581c61f30f6a20184807f056265840
Jared Pines <pinesjared@gmail.com>
2022-05-08 13:43:33 -0700

    Remade database to DungeonCharacter.db because it works better that way

--- src/Model/Database.java
+++ src/Model/Database.java
@@ -41,29 +41,6 @@ public class Database {
             e.printStackTrace();
             System.exit(0);
         }
-        String query1 = "INSERT INTO heroes ( WARRIOR, PRIESTESS, THIEF, OGRE, GREMLIN, SKELETON ) VALUES ( '125', '75', '75', '200', '70', '100' )";
-        String query2 = "INSERT INTO heroes ( WARRIOR, PRIESTESS, THIEF, OGRE, GREMLIN, SKELETON ) VALUES ( '4', '5', '6', '2', '5', '3' )";
-        String query3 = "INSERT INTO heroes ( WARRIOR, PRIESTESS, THIEF, OGRE, GREMLIN, SKELETON ) VALUES ( '0.8', '0.7', '0.8', '0.6', '0.8', '0.8' )";
-        String query4 = "INSERT INTO heroes ( WARRIOR, PRIESTESS, THIEF, OGRE, GREMLIN, SKELETON ) VALUES ( '35', '25', '20', '30', '15', '30' )";
-        String query5 = "INSERT INTO heroes ( WARRIOR, PRIESTESS, THIEF, OGRE, GREMLIN, SKELETON ) VALUES ( '60', '45', '40', '60', '30', '50' )";
-        String query6 = "INSERT INTO heroes ( WARRIOR, PRIESTESS, THIEF, OGRE, GREMLIN, SKELETON ) VALUES ( '0.2', '0.3', '0.4', '0.1', '0.4', '0.3' )";
-        String query7 = "INSERT INTO heroes ( WARRIOR, PRIESTESS, THIEF, OGRE, GREMLIN, SKELETON ) VALUES ( '', '', '', '30', '20', '30' )";
-        String query8 = "INSERT INTO heroes ( WARRIOR, PRIESTESS, THIEF, OGRE, GREMLIN, SKELETON ) VALUES ( '', '', '', '60', '40', '50' )";
-
-
-        try{
-            int myRv = myStmt.executeUpdate(query1);
-            myRv = myStmt.executeUpdate(query2);
-            myRv = myStmt.executeUpdate(query3);
-            myRv = myStmt.executeUpdate(query4);
-            myRv = myStmt.executeUpdate(query5);
-            myRv = myStmt.executeUpdate(query6);
-            myRv = myStmt.executeUpdate(query7);
-            myRv = myStmt.executeUpdate(query8);
-        } catch (SQLException e){
-            e.printStackTrace();
-            System.exit(0);
-        }
     }
     public StringBuilder getStats(String theName) throws SQLException {
         StringBuilder myStats = new StringBuilder();
