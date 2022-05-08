a20ff35bb1397e6317b9dc6ccc6abb62ad2d3546
Jared Pines <pinesjared@gmail.com>
2022-05-08 15:11:04 -0700

    Hero can now be chosen and stats printed to the screen

--- src/Controller/Driver.java
+++ src/Controller/Driver.java
@@ -1,5 +1,19 @@
 package Controller;
 
-public class Driver {
+import Model.Hero;
 
+import java.sql.SQLException;
+
+public class Driver {
+    public static StringBuilder heroToScreen(String theName) throws SQLException {
+        Hero myHero = new Hero(theName);
+        StringBuilder stats = new StringBuilder();
+        stats.append("hit points: ").append(myHero.getHIT_POINTS()).append("\n");
+        stats.append("attack speed: ").append(myHero.getATTACK_SPEED()).append("\n");
+        stats.append("chance to hit: ").append(myHero.getCHANCE_TO_HIT()).append("\n");
+        stats.append("minimum damage: ").append(myHero.getMINIMUM_DAMAGE()).append("\n");
+        stats.append("maximum damage: ").append(myHero.getMAXIMUM_DAMAGE()).append("\n");
+        stats.append("chance to block: ").append(myHero.getCHANCE_TO_BLOCK_OR_HEAL()).append("\n");
+        return stats;
+    }
 }
--- tests/ViewTest/ViewTest.java
+++
@@ -1,8 +0,0 @@
-package ViewTest;
-import org.junit.jupiter.api.Test;
-public class ViewTest {
-    @Test
-    void heroToScreen(){
-
-    }
-}
