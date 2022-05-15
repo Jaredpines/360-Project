c13bebcabb064585eb8c70333fc19e4ca07b453c
emmafedina <emmafedina@gmail.com>
2022-05-14 22:08:44 -0700

    did a  serialization and a cool logo

--- src/Controller/Driver.java
+++ src/Controller/Driver.java
@@ -3,7 +3,7 @@ package Controller;
 import Model.Dungeon;
 import Model.Hero;
 
-import java.io.Serializable;
+import java.io.*;
 import java.sql.SQLException;
 import java.util.Scanner;
 
@@ -68,7 +68,40 @@ public class Driver implements Serializable {
         String myChoice = sc.next();
 
         switch (myChoice) {
+
             case "1":
+                String art = """ 
+          d8888b. db    db d8b   db  d888b  d88888b  .d88b.  d8b   db 
+          88  `8D 88    88 888o  88 88' Y8b 88'     .8P  Y8. 888o  88 
+          88   88 88    88 88V8o 88 88      88ooooo 88    88 88V8o 88 
+          88   88 88    88 88 V8o88 88  ooo 88~~~~~ 88    88 88 V8o88 
+          88  .8D 88b  d88 88  V888 88. ~8~ 88.     `8b  d8' 88  V888 
+          Y8888D' ~Y8888P' VP   V8P  Y888P  Y88888P  `Y88P'  VP   V8P 
+                                                                      
+                                                                      
+   .d8b.  d8888b. db    db d88888b d8b   db d888888b db    db d8888b. d88888b 
+  d8' `8b 88  `8D 88    88 88'     888o  88 `~~88~~' 88    88 88  `8D 88'     
+  88ooo88 88   88 Y8    8P 88ooooo 88V8o 88    88    88    88 88oobY' 88ooooo 
+  88~~~88 88   88 `8b  d8' 88~~~~~ 88 V8o88    88    88    88 88`8b   88~~~~~ 
+  88   88 88  .8D  `8bd8'  88.     88  V888    88    88b  d88 88 `88. 88.     
+  YP   YP Y8888D'    YP    Y88888P VP   V8P    YP    ~Y8888P' 88   YD Y88888P 
+                                                                              
+                                                                              
+                       .d888b.  .d88b.  .d888b. .d888b. 
+                       VP  `8D .8P  88. VP  `8D VP  `8D 
+                          odD' 88  d'88    odD'    odD' 
+                        .88'   88 d' 88  .88'    .88'   
+                       j88.    `88  d8' j88.    j88.    
+                       888888D  `Y88P'  888888D 888888D 
+                                                        
+                        """;
+
+                for (int i = 0; i < art.length(); i++){
+                    System.out.print(art.charAt(i));
+                    Thread.sleep(1);
+                }
+
+
                 System.out.println("Thanks for choosing to play our game");
                 System.out.println("You can always save the game if you need to run and do some errands");
                 System.out.println(chooseHero());
@@ -81,6 +114,42 @@ public class Driver implements Serializable {
                 if(myChoice.equalsIgnoreCase("M")){
                     System.out.println(mapMaker(myX,myY));
                 }
+
+                //HOPEFULLY WHAT WE NEED TO MAKE SERIALIZATION WORK
+                FileOutputStream myFileOut = new FileOutputStream("UserInfo.ser");
+                ObjectOutputStream myOut = new ObjectOutputStream(myFileOut);
+                //This 5 and 5 are just random, still need to figure out what to pass there
+                myOut.writeObject(dungeonToScreen(5,5));
+                myOut.close();
+                myFileOut.close();
+
+                System.out.println("The game info saved");
+
+
+                //This is deserialization
+                Dungeon myDungeon1 = null;
+
+                // Reading the object from a file
+                FileInputStream myFileIn = new FileInputStream("UserInfo.ser");
+                ObjectInputStream myIn = new ObjectInputStream(myFileIn);
+
+                //TODO Serialization works but Deserialization shows an error
+                // (Will ask prof on code review what is wrong)
+                /*
+                // Method for deserialization of object
+                myDungeon1 = (Dungeon) myIn.readObject();
+
+                myIn.close();
+                myFileIn.close();
+
+                System.out.println("Object has been deserialized ");
+                System.out.println("a = " + myDungeon1.getX());
+                System.out.println("b = " + myDungeon1.getY());
+
+
+
+
+                 */
 
 
             case "2":
