8fbb34dfdbf24024346359d7a0d0fd6c5e161a93
Lindsay <lindsayd982@gmail.com>
2022-05-23 05:01:07 -0700

    made attack and special attack for heroes name change in DungeonCharacter

--- src/Model/Warrior.java
+++ src/Model/Warrior.java
@@ -1,19 +1,46 @@
 package Model;
 
 import java.sql.SQLException;
+import java.util.Random;
 
 public class Warrior extends Hero{
     public Warrior(String theName) throws SQLException {
         super(theName);
+        setMyHitPoint(125);
+        setMyAttackSpeed(4);
+        setMyChanceToHit(0.8);
+        setMyChangesToBlockOrHeal(0.2);
+        setMyMaximumDamage(60);
+        setMyMinimumDamage(35);
     }
-    // TODO: Make special attack
-    @Override
+
     String specialAttack() {
-        return null;
+        return "Crushing Blow";
     }
-    // TODO: Make attack
+
+    public void makeSpecialAttack() {
+        attack(75, 175);
+    }
+
     @Override
     public int attack(int theMin, int theMax) {
+//        Random rand = new Random();
+//        int damage = rand.nextInt(theMax - theMin + 1) + theMin;
+//        if (theMin == 75) {
+//            if (Math.random() * 100 < 60 ) {
+//                damage = 0;
+//            }
+//        } else {
+//            if (Math.random() * 100 < (1 - this.getMyChanceToHit()) * 100 ) {
+//            damage = 0;
+//            }
+//        }
+//        return damage;
         return 0;
+    }
+
+    @Override
+    public double blockAttack() {
+        return 0.0;
     }
 }
